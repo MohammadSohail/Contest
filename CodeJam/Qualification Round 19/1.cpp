bf9c34d0b6901d023cf5ceddf3143cb974d9d40d
SohailMohammad <sohailnsu15@gmail.com>
2019-04-11 22:43:48 +0600

    up

--- CodeJam/Qualification Round 19/1.cpp
+++ CodeJam/Qualification Round 19/1.cpp
@@ -6,26 +6,25 @@ int main()
     int T;
     scanf("%d",&T);
     for(int cs = 1; cs <= T; cs++){
-        int N;
-        scanf("%d",&N);
-        string sN = to_string(N);
-        int c = 0;
-        for(int i = 0; i < sN.size(); i++){
-            if(sN[i] == '4')c++;
+
+        cout << "Case #" << cs << ": ";
+
+        string s;
+        cin >> s;
+
+        int n =  s.size();
+
+        vector<int>def(n,0);
+
+        for(int i = 0; i < n; i++){
+            if(s[i] == '4')s[i] = '3',def[i] = 1;
         }
-        if(c == sN.size()){
-            int x = std::stoi(sN);
-            printf("Case #%d: %d %d\n", cs, x/2, x/2);
-        }
-        else{
-            for(int i = 0; i < sN.size(); i++){
-                if(sN[i] == '4'){
-                    sN[i] = '1';
-                }
-            }
-            int x = std::stoi(sN);
-            printf("Case #%d: %d %d\n", cs, x, (N - x));
-        }
+        cout << s << " ";
+
+        int j = 0;
+        for(j = 0; def[j] == 0 && j < n; j++);  ///ignoring the zeros of a string. example: 0000000190
+        for(;j < n; j++)cout << def[j];
+        cout << endl;
 
     }
     return 0;
